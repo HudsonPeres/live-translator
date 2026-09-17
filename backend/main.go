70653f13aa7eccce1ab7cd764fb09ac830cddad6
Hudson Peres <hudsonaap@gmail.com>
2026-09-17 16:53:05 +0100

    fix sslmode via env var

--- backend/main.go
+++ backend/main.go
@@ -580,13 +580,19 @@ func main() {
 		log.Fatal("Variaveis LIVEKIT_API_KEY, LIVEKIT_API_SECRET e LIVEKIT_URL sao obrigatorias")
 	}
 
+		sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		sslmode,
 	)
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
