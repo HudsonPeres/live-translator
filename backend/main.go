19514bcf3262d4b3e85a5191c6977f11af57ff76
Hudson Peres <hudsonaap@gmail.com>
2026-09-17 17:02:08 +0100

    adicionar URL do Vercel ao CORS

--- backend/main.go
+++ backend/main.go
@@ -608,7 +608,7 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://ltc-vert.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
